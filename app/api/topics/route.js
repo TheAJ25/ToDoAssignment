4045dcc035cac0d6055e9ccc3d183b7407eee493
Abhijit Shaw <abhijitshaw2501@gmail.com>
2024-04-11 22:36:41 +0530

    LambdaTimeout problem solved

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -3,21 +3,33 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { title, description } = await request.json();
-  await connectMongoDB();
-  await Topic.create({ title, description });
-  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+  try {
+    const { title, description } = await request.json();
+    await connectMongoDB();
+    await Topic.create({ title, description });
+    return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+  } finally {
+    await mongoose.disconnect();
+  }
 }
 
 export async function GET() {
-  await connectMongoDB();
-  const topics = await Topic.find();
-  return NextResponse.json({ topics })
+  try {
+    await connectMongoDB();
+    const topics = await Topic.find();
+    return NextResponse.json({ topics });
+  } finally {
+    await mongoose.disconnect();
+  }
 }
 
 export async function DELETE(request) {
-  const id = request.nextUrl.searchParams.get('id')
-  await connectMongoDB();
-  await Topic.findByIdAndDelete(id);
-  return NextResponse.json({ message: "Topic Deleted" }, { status: 201 })
+  try {
+    const id = request.nextUrl.searchParams.get('id');
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Topic Deleted" }, { status: 201 });
+  } finally {
+    await mongoose.disconnect();
+  }
 }
