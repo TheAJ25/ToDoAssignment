e2c13750c4354d102cbb602bde3230ea3515a0e1
Abhijit Shaw <abhijitshaw2501@gmail.com>
2024-04-11 23:03:58 +0530

    LambdaTimeout problem solved: 2

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -2,25 +2,28 @@ import connectMongoDB from "@/libs/mongodb";
 import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
+
+connectMongoDB();
+
 export async function PUT(request, { params }) {
   try {
     const { id } = params;
     const { newTitle: title, newDescription: description } = await request.json();
-    await connectMongoDB();
     await Topic.findByIdAndUpdate(id, { title, description });
     return NextResponse.json({ message: "Topic updated" }, { status: 200 });
-  } finally {
-    await mongoose.disconnect();
+  } catch (error) {
+    console.error("Error updating topic:", error);
+    return NextResponse.error({ message: "Failed to update topic" }, { status: 500 });
   }
 }
 
 export async function GET(request, { params }) {
   try {
     const { id } = params;
-    await connectMongoDB();
     const topic = await Topic.findOne({ _id: id });
     return NextResponse.json({ topic }, { status: 200 });
-  } finally {
-    await mongoose.disconnect();
+  } catch (error) {
+    console.error("Error fetching topic:", error);
+    return NextResponse.error({ message: "Failed to fetch topic" }, { status: 500 });
   }
 }
--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -2,34 +2,37 @@ import connectMongoDB from "@/libs/mongodb";
 import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
+
+connectMongoDB();
+
 export async function POST(request) {
   try {
     const { title, description } = await request.json();
-    await connectMongoDB();
     await Topic.create({ title, description });
     return NextResponse.json({ message: "Topic Created" }, { status: 201 });
-  } finally {
-    await mongoose.disconnect();
+  } catch (error) {
+    console.error("Error creating topic:", error);
+    return NextResponse.error({ message: "Failed to create topic" }, { status: 500 });
   }
 }
 
 export async function GET() {
   try {
-    await connectMongoDB();
     const topics = await Topic.find();
     return NextResponse.json({ topics });
-  } finally {
-    await mongoose.disconnect();
+  } catch (error) {
+    console.error("Error fetching topics:", error);
+    return NextResponse.error({ message: "Failed to fetch topics" }, { status: 500 });
   }
 }
 
 export async function DELETE(request) {
   try {
     const id = request.nextUrl.searchParams.get('id');
-    await connectMongoDB();
     await Topic.findByIdAndDelete(id);
     return NextResponse.json({ message: "Topic Deleted" }, { status: 201 });
-  } finally {
-    await mongoose.disconnect();
+  } catch (error) {
+    console.error("Error deleting topic:", error);
+    return NextResponse.error({ message: "Failed to delete topic" }, { status: 500 });
   }
 }
